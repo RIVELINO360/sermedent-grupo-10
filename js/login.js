f153a95bb57587fe21d8a2032764e406911e7087
RIVELINO360 <vilavilarivelino@gmail.com>
2026-05-10 20:46:54 -0500

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -1,4 +1,5 @@
 document.getElementById("loginForm").addEventListener("submit", async function (e) {
+
     e.preventDefault();
 
     const usuario = document.getElementById("usuario").value;
@@ -6,27 +7,43 @@ document.getElementById("loginForm").addEventListener("submit", async function (
     const error = document.getElementById("error");
 
     try {
+
         const response = await fetch("/login", {
+
             method: "POST",
+
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify({ usuario, password })
+
+            body: JSON.stringify({
+                usuario,
+                password
+            })
+
         });
 
         const data = await response.json();
 
         if (data.success) {
+
             localStorage.setItem("login", "true");
             localStorage.setItem("usuario", data.user.nombre_usuario);
             localStorage.setItem("rol", data.user.rol);
+
             window.location.href = "index.html";
+
         } else {
+
             error.textContent = "Usuario o contraseña incorrectos";
+
         }
 
     } catch (err) {
+
         error.textContent = "Error al conectar con el servidor";
         console.log(err);
+
     }
+
 });
